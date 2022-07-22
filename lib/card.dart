25d6d8a06d1349fef6b14b94712db06dba403cd4
CrocoXXX <lawrencenoman@gmail.com>
2022-07-22 18:47:58 +0700

    Finish Custom Card

--- lib/card.dart
+++ lib/card.dart
@@ -23,7 +23,138 @@ class MainCard extends StatelessWidget {
                 end: Alignment.bottomCenter,
               ),
             ),
-          )
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                elevation: 10,
+                child: Stack(
+                  children: [
+                    Opacity(
+                      opacity: 0.5,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          image: const DecorationImage(
+                            image: AssetImage("assets/pattern.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      // width: MediaQuery.of(context).size.width * 0.35,
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(4),
+                          topRight: Radius.circular(4),
+                        ),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://cdn.pixabay.com/photo/2018/01/20/08/33/sunset-3094078_960_720.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                        20,
+                        50 + MediaQuery.of(context).size.height * 0.35,
+                        20,
+                        20,
+                      ),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            const Text(
+                              "Beautiful Sunset at Paddy Field",
+                              maxLines: 2,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Color(0xfff56d7d),
+                                fontSize: 25,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  "Posted on  ",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                Text(
+                                  "July 22, 2022",
+                                  style: TextStyle(
+                                    color: Color(0xfff56d7d),
+                                    fontSize: 12,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Spacer(
+                                  flex: 10,
+                                ),
+                                Icon(
+                                  Icons.thumb_up_sharp,
+                                  color: Colors.grey,
+                                  size: 18,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                Text(
+                                  "1000",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                Spacer(
+                                  flex: 5,
+                                ),
+                                Icon(
+                                  Icons.comment,
+                                  color: Colors.grey,
+                                  size: 18,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                Text(
+                                  "10000",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                Spacer(
+                                  flex: 10,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
